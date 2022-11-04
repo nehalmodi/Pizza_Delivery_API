8ca000e74d5adbfa96147b2c576317bc6a8ee5c1
Nehal Modi <nehalmodi1999@gmail.com>
2022-11-05 02:00:40 +0530

    Signup

--- auth_routes.py
+++ auth_routes.py
@@ -1,10 +1,15 @@
 from fastapi import APIRouter
 from pydantic import BaseModel
+from database import engine, Session
+from schema import SignUpModel
+from models import User
 
 auth_router = APIRouter(
     prefix='/auth',
     tags = ['auth_tag']
 )
+
+session = Session(bind=engine)
 
 class Mul2(BaseModel):
     n: int = 0
@@ -12,4 +17,10 @@ class Mul2(BaseModel):
 @auth_router.get("/")
 async def hello():
     return {"Message" : "Hello World of auth"}
+
+@auth_router.post("/signup")
+async def signup(user: SignUpModel):
+    db_email = session.query(User).filter(User.email==user.email).first()
+
+
 
---
+++ schema.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel
+from typing import Optional
+
+class SignUpModel(BaseModel):
+    id: Optional[int]
+    username: str
+    email: str
+    password: str
+    is_staff: Optional[bool]
+    is_active: Optional[bool]
+
+    class Config:
+        orm_mode = True
