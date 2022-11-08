68e8338340c4a0acc6b5d0b582848b3c9a7fb351
Nehal Modi <nehalmodi1999@gmail.com>
2022-11-09 00:34:36 +0530

    Added Models

--- schema.py
+++ schema.py
@@ -12,12 +12,37 @@ class SignUpModel(BaseModel):
     class Config:
         orm_mode = True
 
-        schema_extra={
+        schema_extra = {
             "example": {
                 "username": "nehal_modi",
                 "email": "nehalmodi1999@gmail.com",
                 "password": "password",
                 "is_staff": False,
                 "is_active": True
+            }
+        }
+
+class Settings(BaseModel):
+    authjwt_secret_key:str = '8a9b2b07f496fd5155b88da0b0960f23d871c1ff8dd5bf939063a5b0e14fe72b'
+
+
+class LoginModel(BaseModel):
+    username:str
+    password:str
+
+class OrderModel(BaseModel):
+    id:Optional[int]
+    quantity: int
+    order_status:Optional[str]="PENDING"
+    pizza_size:Optional[str] ="SMALL"
+    user_id: Optional[int]
+
+    class Config:
+        orm_mode = True
+
+        schema_extra = {
+            "example": {
+                "quantity": "2",
+                "pizza_size": "LARGE"
             }
         }
